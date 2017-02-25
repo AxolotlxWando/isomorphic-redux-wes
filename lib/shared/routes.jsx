599a827b550c4dfe2c59d7a2c760eaa03e08d2ca
Wesley Wang <zakudesune@gmail.com>
2017-02-26 04:22:45 +1100

    add missing public folder to .gitignore; fix client routes root path to include deploymentURLMapping

--- lib/shared/routes.jsx
+++ lib/shared/routes.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Route, IndexRoute } from 'react-router'
 
+import config from '../../config'
 import App from 'lib/shared/components'
 import Home from 'lib/shared/components/Home'
 
 export default (
-  <Route name='app' component={App} path='/' >
+  <Route name='app' component={App} path={config.deploymentURLMapping} >
     <IndexRoute component={Home} />
   </Route>
 )
