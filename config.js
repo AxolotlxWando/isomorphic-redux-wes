576d7be9bc1fbcb20b594d8b19edecf2b4a2ef9a
Wesley Wang <zakudesune@gmail.com>
2017-02-26 02:54:54 +1100

    add content base configuability through deploymentURLMapping config entry (in particular for scenarios where app is reverse-proxied); add sample rc config file

--- config.js
+++ config.js
@@ -1,6 +1,9 @@
 var rc = require('rc');
+var path = require('path')
 
 module.exports = rc('isomorphic-redux-wes', {
   port: process.env.PORT || 3001,
-  devPort: process.env.DEV_PORT || 8001
+  devPort: process.env.DEV_PORT || 8001,
+  /* For resolving api calls and static assets */
+  deploymentURLMapping: process.env.DEPLOYMENT_URL_MAPPING || '/'
 });
